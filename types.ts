96028885ce3f2324e3130dd3985a2337d6c12d59
Valeria Gabrielli <gabvale53@gmail.com>
2026-06-30 13:14:36 +0200

    push finale

---
+++ types.ts
@@ -0,0 +1,11 @@
+export type Product = {
+    title: string;
+    category: string;
+    price: number;
+    description: string;
+    ingredients?: string;
+    dangerLevel: string;
+    img:string;
+    categoryImg: string;
+    
+};
